fa5a56166971d8f951ee890afc0a93e985cbf9b3
g t vasu <papathambi143@gmail.com>
2026-08-01 17:22:24 +0530

    Time: 1 ms (89.75%) | Memory: 42.8 MB (15.76%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0){
+        
+             return false;
+        }
+        while(n%4==0){
+            
+    
+            
+             n=n/4;
+            }
+        
+    
+         return n==1;
+    
+         
+   
+    }  
+    
+}
