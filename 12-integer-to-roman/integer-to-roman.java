181c1acdb4f16dc8b28cd369e031e1e85ad25f3b
g t vasu <papathambi143@gmail.com>
2026-08-31 08:48:55 +0530

    Time: 5 ms (20.15%) | Memory: 46.5 MB (25.57%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String intToRoman(int num) {
+       int[] v={ 1000,900,500,400,100,90,50,40,10,9,5,4,1};
+       String[] rom={"M", "CM", "D", "CD", "C", "XC",
+            "L", "XL", "X", "IX", "V", "IV", "I"};
+            String s="";
+            for(int i=0;i<v.length;i++){
+                while(num>=v[i]){
+                    s += rom[i];
+                     num -= v[i];
+
+                }
+            }
+            return s;
+    }
+}
