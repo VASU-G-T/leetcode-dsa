a413250983a85df6b0e8ca0a1835f01050dcb649
g t vasu <papathambi143@gmail.com>
2026-09-16 22:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (91.95%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int c=0;
+        char[] a=jewels.toCharArray();
+        char[] b=stones.toCharArray();
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<b.length;j++){
+                if(a[i]==b[j]){
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
